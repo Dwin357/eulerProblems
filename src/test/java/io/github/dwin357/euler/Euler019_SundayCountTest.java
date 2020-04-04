e66162b1bf888149f64a0192a033a95ecd115796
Dwin <dwin.e357@gmail.com>
2020-04-04 13:47:06 -0400

    Adds .gitignore

--- src/test/java/io/github/dwin357/euler/Euler019_SundayCountTest.java
+++ src/test/java/io/github/dwin357/euler/Euler019_SundayCountTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.*;
  *
  * How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
  *
- * 
+ *
  */
 
 public class Euler019_SundayCountTest {
