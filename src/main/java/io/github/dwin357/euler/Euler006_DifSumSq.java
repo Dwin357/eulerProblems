b12f0b7599a1948bb31c0ee99eb3460ce84bbfc2
Dwin <dwin.e357@gmail.com>
2020-04-11 22:18:11 -0400

    Adds solution to problem 6

--- src/main/java/io/github/dwin357/euler/Euler006_DifSumSq.java
+++ src/main/java/io/github/dwin357/euler/Euler006_DifSumSq.java
@@ -1,5 +1,7 @@
 package io.github.dwin357.euler;
 
+import java.util.function.IntFunction;
+
 /*
  * so...  this really sounds like huberis talking, but this doesn't seem that bad to me
  * go from x -> y maintaining 2 running totals :: += num & += (num * num)
@@ -7,6 +9,19 @@ package io.github.dwin357.euler;
  */
 public class Euler006_DifSumSq {
     public long difSum(int st, int fin) {
-        return -1;
+        long sum = sumNum(st, fin, i -> {return i;});
+        long sqSum = sum * sum;
+        long sumSq = sumNum(st, fin, i ->{ return i * i;});
+        return sqSum - sumSq;
     }
+
+    public long sumNum(int st, int fin, IntFunction<Integer> f) {
+        long runningTotal = 0;
+        for(int i=st; i<=fin; i++) {
+            runningTotal += f.apply(i);
+        }
+        return runningTotal;
+    }
+
+
 }
--- src/test/java/io/github/dwin357/euler/Euler006_DifSumSqTest.java
+++ src/test/java/io/github/dwin357/euler/Euler006_DifSumSqTest.java
@@ -3,6 +3,8 @@ package io.github.dwin357.euler;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.function.IntFunction;
+
 import static org.junit.Assert.*;
 
 /*
@@ -32,16 +34,90 @@ public class Euler006_DifSumSqTest {
         assertEquals(expected, actual);
     }
 
-    @Ignore
     @Test
-    public void givenExample_1to100_isUnknown() {
+    public void givenProblem_1to100_isUnknown() {
         int st = 1;
         int fin = 100;
-        long expected = 2640L;
+        long expected = 25164150;
         Euler006_DifSumSq impl = new Euler006_DifSumSq();
 
         long actual = impl.difSum(st, fin);
 
         assertEquals(expected, actual);
     }
+
+///////////  By Hand Impl  ///////////////
+    @Test
+    public void sumNum_simpleAdd_100() {
+        long expected = 5050;
+        int st = 1;
+        int fin = 100;
+        IntFunction<Integer> f = (i) -> { return i; };
+        Euler006_DifSumSq impl = new Euler006_DifSumSq();
+
+        long actual = impl.sumNum(st, fin, f);
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void sumNum_simpleAdd() {
+        long expected = 55;
+        int st = 1;
+        int fin = 10;
+        IntFunction<Integer> f = (i) -> { return i; };
+        Euler006_DifSumSq impl = new Euler006_DifSumSq();
+
+        long actual = impl.sumNum(st, fin, f);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void sumNum_simpleSquare() {
+        long expected = 385;
+        int st = 1;
+        int fin = 10;
+        IntFunction<Integer> f = (i) -> { return i * i; };
+        Euler006_DifSumSq impl = new Euler006_DifSumSq();
+
+        long actual = impl.sumNum(st, fin, f);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void sumNum_simpleSquare_100() {
+        long expected = 338350;
+        int st = 1;
+        int fin = 100;
+        IntFunction<Integer> f = (i) -> { return i * i; };
+        Euler006_DifSumSq impl = new Euler006_DifSumSq();
+
+        long actual = impl.sumNum(st, fin, f);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void square_100() {
+        long expected = 25_502_500L;
+        long given = 5050;
+
+        long actual = given * given;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void simple_by_hand() {
+        long expected = 25164150; // this was right...  but is there a better way
+
+        long sumSquared = 25_502_500L;
+        long squaresSummed = 338350L;
+
+        long actual = 25_502_500L - 338350L;
+
+        assertEquals(expected, actual);
+    }
+
+
 }
